56ed129d99755764f3ce3fc4c9e0708833013675
keith66fuller <keith66fuller@gmail.com>
2018-06-27 18:18:30 -0500

    arghhh

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let slot;
+    var fbSuppress = false;
+
+    const game = {
+        Rock: 2,
+        Scissors: 1,
+        Paper: 0
+    }
     const playerNums = [1, 2];
     const config = {
         apiKey: "AIzaSyBlpgyV1NYKnitdSRUCP83Bb1Sl6qWqEQ0",
@@ -16,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
         name: null,
         win: 0,
         lose: 0,
+        draw: 0,
         slot: null
     }
 
@@ -35,29 +42,41 @@ document.addEventListener('DOMContentLoaded', function () {
     const connectedRef = db.ref(".info/connected");
 
 
-      window.addEventListener("beforeunload", function (event) {
+    window.addEventListener("beforeunload", function (event) {
         console.log(`Before Unload ${event}`)
         console.log(JSON.stringify(player))
         if (player.slot) {
             db.ref(`players/${player.slot}`).remove()
-            .then(function() {
-              console.log("Remove succeeded.")
-            })
-            .catch(function(error) {
-              console.log("Remove failed: " + error.message)
-            });
+                .then(function () {
+                    console.log("Remove succeeded.")
+                })
+                .catch(function (error) {
+                    console.log("Remove failed: " + error.message)
+                });
         }
-      });
+    });
 
-    let players = db.ref('players');
-
-    console.log(`PLAYERS: ${players}`)
-
-    pRef = players.child('1')
-
-    console.log(`QUERY: ${pRef.orderByKey()}`)
+    $('.gameButton').on('click', function () {
+        console.log($(this).text())
+        console.log($(this).parent().prop('id'))
+        let playerNum = $(this).parent().prop('id').toString().replace('player', '')
+        console.log(`player ${playerNum} ${player.slot}`)
+        if (playerNum == player.slot) {
+            dbRef.child('players').child(playerNum).update({ choice: $(this).text() })
+                .then(e => {
+                    console.log(`60 ${e}`)
+                })
+                .catch(e => {
+                    console.log(`63 ${e}`)
+                })
+        }
+    })
 
     db.ref().on('value', (snapshot) => {
+        if (fbSuppress) {
+            fbSuppress = false;
+            return;
+        }
         playerNums.forEach(num => {
             console.log(`NUM ${num}`)
             if (snapshot.child(`players/${num}`).exists()) {
@@ -70,69 +89,57 @@ document.addEventListener('DOMContentLoaded', function () {
             } else {
                 console.log("NOT EXISTS")
                 $('#welcome').empty()
-                .append(
-                    $('<input type="text" name="playerName" id="playerName" placeholder="Name">')
-                )
-                .append(
-                    $('<button id="startButton">').text("Start")
-                )
-        
+                    .append(
+                        $('<input type="text" name="playerName" id="playerName" placeholder="Name">')
+                    )
+                    .append(
+                        $('<button id="startButton">').text("Start")
+                    )
+
                 $(`#${num}.nameTag`).text(`Waiting for Player ${num}`)
                 $(`#player${num} .gameButton`).hide();
                 $(`#player${num} .stats`).hide();
             }
+            if (snapshot.child(`players/1`).exists() && snapshot.child(`players/2`).exists()) {
+                console.log(snapshot.child('players/1/choice').val() + ' ' + snapshot.child('players/2/choice').val())
+                let choice = {}
+                if (snapshot.child('players/1/choice').val() && snapshot.child('players/2/choice').val()) {
+                    choice[1] = game[snapshot.child('players/1/choice').val()]
+                    choice[2] = game[snapshot.child('players/2/choice').val()]
+
+                    if (choice[1] == choice[2]) {
+                        [1, 2].forEach(num => {
+                            dbRef.child(`players/${num}/choice`).transaction(() => {
+                                return null;
+                            }, null, false);
+                        });
+
+                        [1, 2].forEach(num => {
+                            console.log(`NUM 105: ${num}`)
+                            firebase.database().ref(`players/${num}/draw`).transaction((draws) => {
+                                fbSuppress = true;
+                                return ++draws;
+                            }, null, false);
+
+                        })
+                    }
+                }
+            }
         });
-
-
-
-        // if (snapshot.val().inProgress) {
-        //     // $('#p2_name').empty().text("Waiting for Player FOO")
-        //     // <input type="text" name="name" id="name" placeholder="Name">
-        //     // <button>Start</button>
-        //     $('#welcome').empty()
-        //         .append(
-        //             $('<input type="text" name="playerName" id="playerName" placeholder="Name">')
-        //         )
-        //         .append(
-        //             $('<button id="startButton">').text("Start")
-        //         )
-        // }
     })
 
-    // db.ref('/global/inProgress').on('value', function (snapshot) {
-    //     console.log(`CHANGED: ${snapshot.val()}`)
-    // });
-
-    // db.ref('/global/inProgress').once('value').then(function (snapshot) {
-    //     console.log(`SNAPSHOT: ${JSON.stringify()}`)
-    //     console.log(`IN PROGRESS: ${snapshot.val()}`)
-    // });
-
-    // function setPlayerName(playerNum) {
-    //     let args = {};
-    //     args[`p${playerNum}_name`] = $('#playerName').val();
-    //     db.ref().update(args).then(function () {
-    //         console.log("Document successfully updated!");
-    //     })
-    //         .catch(function (error) {
-    //             // The document probably doesn't exist.
-    //             console.error(`Error updating document for player ${playerNum}: `, error);
-    //         });
-    // }
 
     // Event fires when user clicks Start after having put in his name.
     $('#welcome').on('keyup', '#playerName', (e) => {
         event.preventDefault();
-        // Number 13 is the "Enter" key on the keyboard
         if (event.keyCode === 13) {
-            // Trigger the button element with a click
             document.getElementById("startButton").click();
         }
     })
     $('#welcome').on('click', '#startButton', (e) => {
         player.name = $('#playerName').val();
         dbRef.once('value', snapshot => {
-            if (!snapshot.hasChild('players')) {
+            if (!snapshot.hasChild('players/1')) {
                 player.slot = 1;
             } else if (!snapshot.child('players/2').exists()) {
                 player.slot = 2;
@@ -142,19 +149,20 @@ document.addEventListener('DOMContentLoaded', function () {
             dbRef.child('players').child(player.slot).set({
                 name: player.name,
                 win: 0,
-                lose: 0
+                lose: 0,
+                draw: 0
             })
-            .catch(e => {
-                console.log(e);
-            });
+                .catch(e => {
+                    console.log(e);
+                });
             console.log('HERE')
             $('#welcome').empty()
-            .append(
-                $('<h3>').text(`Hi ${player.name}!  You are Player ${player.slot}`)
-            )
-            .append(
-                $('<h3 id="gameMsg">')
-            );
+                .append(
+                    $('<h3>').text(`Hi ${player.name}!  You are Player ${player.slot}`)
+                )
+                .append(
+                    $('<h3 id="gameMsg">')
+                );
         })
 
 
